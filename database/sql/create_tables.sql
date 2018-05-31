052d0dd20f7b4196b35ff05acedbb012e1a31604
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-05-31 11:22:21 -0300

    Criar tabelas Cozinheira e Reparador

--- database/sql/create_tables.sql
+++ database/sql/create_tables.sql
@@ -1,8 +1,12 @@
+DROP TABLE IF EXISTS Reparador CASCADE;
+DROP TABLE IF EXISTS Cozinheira CASCADE;
 DROP TABLE IF EXISTS nutricionista CASCADE;
 DROP TABLE IF EXISTS morador CASCADE;
+DROP TABLE IF EXISTS PessoaTelefone;
 DROP TABLE IF EXISTS Pessoa CASCADE;
 
-CREATE TABLE Pessoa (
+CREATE TABLE Pessoa
+(
     cpf VARCHAR(11) NOT NULL,
     sexo VARCHAR NOT NULL,
     rg VARCHAR(10) NOT NULL,
@@ -12,6 +16,15 @@ CREATE TABLE Pessoa (
     email VARCHAR(100),
 
     CONSTRAINT pk_pessoa PRIMARY KEY (cpf)
+);
+
+CREATE TABLE PessoaTelefone
+(
+    cpf_pessoa VARCHAR(11) NOT NULL,
+    telefone VARCHAR(11) NOT NULL,
+
+    CONSTRAINT pessoatelefone_pk PRIMARY KEY (cpf_pessoa, telefone),
+    CONSTRAINT pessoatelefone_fk_pessoa FOREIGN KEY (cpf_pessoa) REFERENCES Pessoa
 );
 
 -- Table: morador
@@ -38,3 +51,22 @@ CREATE TABLE nutricionista
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
  
+-- Table: cozinheira
+
+CREATE TABLE Cozinheira
+(
+    cpf_pessoa VARCHAR(11) NOT NULL,
+
+    CONSTRAINT cozinheira_pk PRIMARY KEY (cpf_pessoa),
+    CONSTRAINT cozinheira_pk_pessoa FOREIGN KEY (cpf_pessoa) REFERENCES Pessoa (cpf)
+);
+
+-- Table: reparador
+
+CREATE TABLE Reparador
+(
+    cpf_pessoa VARCHAR(11) NOT NULL,
+
+    CONSTRAINT reparador_pk PRIMARY KEY (cpf_pessoa),
+    CONSTRAINT reparador_fk_pessoa FOREIGN KEY (cpf_pessoa) REFERENCES Pessoa
+);
