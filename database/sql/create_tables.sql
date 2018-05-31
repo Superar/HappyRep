772eaebd7bacee1445cea3d91873cc41f78a69aa
Tiago Bachiega de Almeida <tiago_almeida0297@hotmail.com>
2018-05-31 00:07:03 -0300

    Update create_tables.sql
    Adicionadas as tabelas para Nutricionista e Morador.

--- database/sql/create_tables.sql
+++ database/sql/create_tables.sql
@@ -1,4 +1,6 @@
-﻿DROP TABLE IF EXISTS Pessoa CASCADE;
+DROP TABLE IF EXISTS nutricionista CASCADE;
+DROP TABLE IF EXISTS morador CASCADE;
+DROP TABLE IF EXISTS Pessoa CASCADE;
 
 CREATE TABLE Pessoa (
     cpf VARCHAR(11) NOT NULL,
@@ -11,3 +13,28 @@ CREATE TABLE Pessoa (
 
     CONSTRAINT pk_pessoa PRIMARY KEY (cpf)
 );
+
+-- Table: morador
+
+CREATE TABLE morador
+(
+  trabalho character varying(50),
+  universidade character varying(50),
+  cpf_pessoa character varying(11) NOT NULL,
+  CONSTRAINT morador_pk PRIMARY KEY (cpf_pessoa),
+  CONSTRAINT morador_fk_pessoa FOREIGN KEY (cpf_pessoa)
+      REFERENCES public.pessoa (cpf) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+-- Table: nutricionista
+
+CREATE TABLE nutricionista
+(
+  cpf_pessoa character varying(11) NOT NULL,
+  CONSTRAINT nutricionista_pk PRIMARY KEY (cpf_pessoa),
+  CONSTRAINT nutricionista_pk_pessoa FOREIGN KEY (cpf_pessoa)
+      REFERENCES public.pessoa (cpf) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+ 
