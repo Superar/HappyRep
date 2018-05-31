ec3e3b08210dab4a03061d7ed12d6421a9bc1c84
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-05-31 15:55:51 -0300

    Criação do formulário de cadastro de pessoa

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HappyRep' });
 });
 
+router.get('/CadastrarReparador', function(req, res) {
+  res.render('formularios/cadastrar_pessoa', {pessoa: 'Reparador'});
+});
+
 module.exports = router;
