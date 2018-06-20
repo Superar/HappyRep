44864c5771eec67129b2e3f172790420e9586457
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-06-19 22:03:44 -0300

    Funções de alteraçao do tipo do reparador

--- database/sql/create_triggers.sql
+++ database/sql/create_triggers.sql
@@ -1,3 +1,6 @@
+-- Insere reparador em view_reparador
+-- Autor: Marcio Lima Inácio
+
 CREATE OR REPLACE FUNCTION insert_view_reparador() RETURNS trigger AS $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = NEW.cpf) THEN
@@ -15,3 +18,27 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER insert_view_reparador INSTEAD OF INSERT ON view_reparador
 FOR EACH ROW EXECUTE PROCEDURE insert_view_reparador();
+
+-- Deleta linha em view_reparador
+-- Autor: Marcio Lima Inácio
+
+CREATE OR REPLACE FUNCTION delete_row_view_reparador() RETURNS trigger AS $$
+DECLARE
+    _num_rows INT;
+BEGIN
+    SELECT COUNT(1) INTO _num_rows FROM view_reparador vr WHERE vr.cpf = OLD.cpf;
+
+    -- Se for a ultima linha a ser deletada, deve deletar o reparador
+    IF _num_rows = 1 THEN
+        DELETE FROM ReparadorTipo rt WHERE rt.cpf_reparador = OLD.cpf AND rt.tipo = OLD.tipo;
+        DELETE FROM Reparador r WHERE r.cpf_pessoa = OLD.cpf;
+    -- Caso contrário, deleta-se apenas o tipo
+    ELSE
+        DELETE FROM ReparadorTipo rt WHERE rt.cpf_reparador = OLD.cpf AND rt.tipo = OLD.tipo; 
+    END IF;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_row_view_reparador INSTEAD OF DELETE ON view_reparador
+FOR EACH ROW EXECUTE PROCEDURE delete_row_view_reparador();
