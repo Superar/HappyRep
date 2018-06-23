9f517772fe1034c644bcc5ab9941943c70754552
lftomazini <lftomazini@hotmail.com>
2018-06-23 18:27:39 -0300

    Changed function names do reflect behaviour

--- database/sql/get_data_functions.sql
+++ database/sql/get_data_functions.sql
@@ -1,80 +1,8 @@
---Pega dados do Nutricionista
---Autor: Tiago Bachiega de Almeida
-
---sexo
-CREATE OR REPLACE FUNCTION get_nutricionista_sexo(_cpf VARCHAR) RETURNS VARCHAR(11) AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
-        RETURN (SELECT sexo FROM Pessoa p WHERE p.cpf = _cpf);
-    ELSE
-        RETURN (0);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
---rg
-CREATE OR REPLACE FUNCTION get_nutricionista_rg(_cpf VARCHAR) RETURNS VARCHAR(10) AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
-        RETURN (SELECT rg FROM Pessoa p WHERE p.cpf = _cpf);
-    ELSE
-        RETURN (0);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
---nome_prenome
-CREATE OR REPLACE FUNCTION get_nutricionista_prenome(_cpf VARCHAR) RETURNS VARCHAR(30) AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
-        RETURN (SELECT nome_prenome FROM Pessoa p WHERE p.cpf = _cpf);
-    ELSE
-        RETURN (0);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
---nome_sobrenome
-CREATE OR REPLACE FUNCTION get_nutricionista_sobrenome(_cpf VARCHAR) RETURNS VARCHAR(70) AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
-        RETURN (SELECT nome_sobrenome FROM Pessoa p WHERE p.cpf = _cpf);
-    ELSE
-        RETURN (0);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
---data_de_nascimento
-CREATE OR REPLACE FUNCTION get_nutricionista_data(_cpf VARCHAR) RETURNS DATE AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
-        RETURN (SELECT data_de_nascimento FROM Pessoa p WHERE p.cpf = _cpf);
-    ELSE
-        RETURN (0);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
---email
-CREATE OR REPLACE FUNCTION get_nutricionista_email(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
-        RETURN (SELECT email FROM Pessoa p WHERE p.cpf = _cpf);
-    ELSE
-        RETURN (0);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
-
---Pega dados do Morador
+--Pega dados de pessoas e seus derivados
 --Autor: Tiago Bachiega de Almeida
 
 --trabalho
-CREATE OR REPLACE FUNCTION get_morador_trabalho(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
+CREATE OR REPLACE FUNCTION get_trabalho(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Morador m WHERE m.cpf_pessoa = _cpf) THEN
         RETURN (SELECT trabalho FROM Morador m WHERE m.cpf_pessoa = _cpf);
@@ -85,7 +13,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --universidade
-CREATE OR REPLACE FUNCTION get_morador_universidade(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
+CREATE OR REPLACE FUNCTION get_universidade(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Morador m WHERE m.cpf_pessoa = _cpf) THEN
         RETURN (SELECT universidade FROM Morador m WHERE m.cpf_pessoa = _cpf);
@@ -96,7 +24,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --sexo
-CREATE OR REPLACE FUNCTION get_morador_sexo(_cpf VARCHAR) RETURNS VARCHAR(11) AS $$
+CREATE OR REPLACE FUNCTION get_sexo(_cpf VARCHAR) RETURNS VARCHAR(11) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
         RETURN (SELECT sexo FROM Pessoa p WHERE p.cpf = _cpf);
@@ -107,7 +35,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --rg
-CREATE OR REPLACE FUNCTION get_morador_rg(_cpf VARCHAR) RETURNS VARCHAR(10) AS $$
+CREATE OR REPLACE FUNCTION get_rg(_cpf VARCHAR) RETURNS VARCHAR(10) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
         RETURN (SELECT rg FROM Pessoa p WHERE p.cpf = _cpf);
@@ -118,7 +46,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --nome_prenome
-CREATE OR REPLACE FUNCTION get_morador_prenome(_cpf VARCHAR) RETURNS VARCHAR(30) AS $$
+CREATE OR REPLACE FUNCTION get_prenome(_cpf VARCHAR) RETURNS VARCHAR(30) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
         RETURN (SELECT nome_prenome FROM Pessoa p WHERE p.cpf = _cpf);
@@ -129,7 +57,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --nome_sobrenome
-CREATE OR REPLACE FUNCTION get_morador_sobrenome(_cpf VARCHAR) RETURNS VARCHAR(70) AS $$
+CREATE OR REPLACE FUNCTION get_sobrenome(_cpf VARCHAR) RETURNS VARCHAR(70) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
         RETURN (SELECT nome_sobrenome FROM Pessoa p WHERE p.cpf = _cpf);
@@ -140,7 +68,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --data_de_nascimento
-CREATE OR REPLACE FUNCTION get_morador_data(_cpf VARCHAR) RETURNS DATE AS $$
+CREATE OR REPLACE FUNCTION get_data(_cpf VARCHAR) RETURNS DATE AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
         RETURN (SELECT data_de_nascimento FROM Pessoa p WHERE p.cpf = _cpf);
@@ -151,7 +79,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --email
-CREATE OR REPLACE FUNCTION get_morador_email(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
+CREATE OR REPLACE FUNCTION get_email(_cpf VARCHAR) RETURNS VARCHAR(100) AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = _cpf) THEN
         RETURN (SELECT email FROM Pessoa p WHERE p.cpf = _cpf);
