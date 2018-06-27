414f08decc252d4edb09b72b0dbaf0bca500392d
TiagoB936 <tiago_almeida0297@hotmail.com>
2018-06-27 18:55:08 -0300

    novos triggers e views

--- database/sql/create_triggers.sql
+++ database/sql/create_triggers.sql
@@ -64,5 +64,24 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER insert_view_nutricionista INSTEAD OF INSERT ON view_nutricionista
 FOR EACH ROW EXECUTE PROCEDURE insert_view_nutricionista();
 
+-- Insere morador em view morador
+-- Autor: Tiago Bachiega de Almeida
 
+--function
+CREATE OR REPLACE FUNCTION insert_view_morador() RETURNS trigger AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = NEW.cpf) THEN
+        INSERT INTO Pessoa VALUES (NEW.cpf, NEW.sexo, NEW.rg, NEW.nome_prenome, NEW.nome_sobrenome, NEW.data_de_nascimento, NEW.email);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Nutricionista n WHERE n.cpf_pessoa = NEW.cpf) THEN
+        INSERT INTO Morador VALUES (NEW.trabalho, NEW.universidade, NEW.cpf);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--trigger respectivo
+
+CREATE TRIGGER insert_view_morador INSTEAD OF INSERT ON view_morador
+FOR EACH ROW EXECUTE PROCEDURE insert_view_morador();
 
