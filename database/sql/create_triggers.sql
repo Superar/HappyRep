e99e5e3cb76b25b47aee5936a3c0fb7bcbd8ab9a
Isadora <isadoragallerani@hotmail.com>
2018-07-10 17:22:46 -0300

    Triggers de faxina, alimentacao e ingredientes

--- database/sql/create_triggers.sql
+++ database/sql/create_triggers.sql
@@ -1,4 +1,4 @@
--- Insere reparador em view_reparador
+﻿-- Insere reparador em view_reparador
 -- Autor: Marcio Lima Inácio
 
 CREATE OR REPLACE FUNCTION insert_view_reparador() RETURNS trigger AS $$
@@ -196,3 +196,78 @@ $valida_morador$ LANGUAGE plpgsql;
 
 CREATE TRIGGER valida_morador BEFORE INSERT OR UPDATE ON Morador
     FOR EACH ROW EXECUTE PROCEDURE valida_morador();
+
+-- Insere Faxina
+-- Autor: Isadora Gallerani
+
+CREATE OR REPLACE FUNCTION insert_view_faxina() RETURNS trigger AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = NEW.cpf) THEN
+        PERFORM insert_pessoa(NEW.cpf, NEW.sexo, NEW.rg, NEW.nome_prenome, NEW.nome_sobrenome, NEW.data_de_nascimento, NEW.email);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Faxineira fx WHERE fx.cpf_pessoa = NEW.cpf) THEN
+        PERFORM insert_faxineira VALUES (NEW.cpf);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Servico s WHERE s.id_servico = NEW.id_servico) THEN
+        PERFORM insert_servico (NEW.id_servico, NEW.hora_inicio, NEW.hora_fim);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Faxina f WHERE f.cpf_faxineira = NEW.cpf AND f.id_servico = NEW.id_servico) THEN
+        INSERT INTO Faxina VALUES (NEW.cpf, NEW.id_servico);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_view_faxina INSTEAD OF INSERT ON view_faxina
+FOR EACH ROW EXECUTE PROCEDURE insert_view_faxina();
+
+-- Insere Ingredientes
+-- Autor: Isadora Gallerani
+
+CREATE OR REPLACE FUNCTION insert_view_ingredientes() RETURNS trigger AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Receita r WHERE r.id_receita = NEW.id_receita) THEN
+        PERFORM insert_receita(NEW.id_receita, NEW.nome_produto, NEW.descricao_receita);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Produto pr WHERE pr.id_produto = NEW.id_produto) THEN
+        PERFORM insert_produto VALUES (NEW.id_produto, NEW.descricao, NEW.marca, NEW.categoria, NEW.nome);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Ingredientes i WHERE i.id_receita = NEW.id_receita AND i.id_produto = NEW.id_produto) THEN
+        INSERT INTO Ingredientes VALUES (NEW.id_receita, NEW.id_produto);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_view_ingredientes INSTEAD OF INSERT ON view_ingredientes
+FOR EACH ROW EXECUTE PROCEDURE insert_view_ingredientes();
+
+-- Insere Alimentação
+-- Autor: Isadora Gallerani
+
+CREATE OR REPLACE FUNCTION insert_view_alimentacao() RETURNS trigger AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = NEW.cpf_cozinheira) THEN
+        PERFORM insert_pessoa(NEW.cpf, NEW.sexo, NEW.rg, NEW.nome_prenome, NEW.nome_sobrenome, NEW.data_de_nascimento, NEW.email);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Pessoa p WHERE p.cpf = NEW.cpf_nutricionista) THEN
+        PERFORM insert_pessoa(NEW.cpf, NEW.sexo, NEW.rg, NEW.nome_prenome, NEW.nome_sobrenome, NEW.data_de_nascimento, NEW.email);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Cozinheira c WHERE c.cpf_pessoa = NEW.cpf_cozinheira) THEN
+        PERFORM insert_cozinheira(NEW.cpf_cozinheira);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Nutricionista n WHERE n.cpf_pessoa = NEW.cpf_nutricionista) THEN
+        PERFORM insert_nutricionista(NEW.cpf_nutricionista);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Servico s WHERE s.id_servico = NEW.id_servico) THEN
+        PERFORM insert_servico(NEW.id_servico, NEW.hora_inicio, NEW.hora_fim);
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM Alimentacao a WHERE a.id_servico = NEW.id_servico) THEN
+        INSERT INTO Alimentacao VALUES (NEW.id_servico, NEW.cpf_cozinheira, NEW.cpf_nutricionista);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_view_alimentacao INSTEAD OF INSERT ON view_alimentacao
+FOR EACH ROW EXECUTE PROCEDURE insert_view_alimentacao();
